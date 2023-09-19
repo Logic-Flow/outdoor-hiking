7e6ae673bc2895847b0b0d4feb86415f92200cc7
zhangmo <zhangmo@didiglobal.com>
2023-09-19 17:39:19 +0800

    fix: 文件名称

--- sites/logicflow-docs/.dumirc.ts
+++ sites/logicflow-docs/.dumirc.ts
@@ -9,14 +9,16 @@ export default defineConfig({
     rtl: true,
     nav: {
       'zh-CN': [
-        { title: '文档', link: '/tutorial/tutorial' },
-        { title: 'API', link: '/api/api' },
-        { title: '示例', link: '/examples/examples' },
+        { title: '文档', link: '/tutorial' },
+        { title: 'API', link: '/api' },
+        { title: '示例', link: '/examples' },
+        { title: '文章', link: '/article' },
       ],
       'en-US': [
-        { title: 'Tutorial', link: '/en-US/tutorial/tutorial' },
-        { title: 'API', link: '/en-US/api/api' },
-        { title: 'Examples', link: '/en-US/examples/examples' },
+        { title: 'Tutorial', link: '/en-US/tutorial' },
+        { title: 'API', link: '/en-US/api' },
+        { title: 'Examples', link: '/en-US/examples' },
+        { title: 'Article', link: '/en-US/article' },
       ],
     },
     socialLinks: {
