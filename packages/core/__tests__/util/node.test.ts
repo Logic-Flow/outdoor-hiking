273d8a8be6f3c35dbe97465a339d72c94613ddc1
jerryjiang <jerryjiang@didiglobal.com>
2023-12-04 15:34:23 +0800

    feat(core): tests for core

--- packages/core/__tests__/util/node.test.ts
+++ packages/core/__tests__/util/node.test.ts
@@ -1,17 +1,68 @@
+import {
+  getClosestAnchor,
+  isInNode,
+  getCrossPointWithCircle,
+  getCrossPointWithEllipse,
+} from '../../src/util/node';
+
 describe('util/node', () => {
-  test('get the direction of a point to the center of the bbox', () => {})
-  test('get the bbox of node', () => {})
-  test('get the points on expand bbox', () => {})
-  test('get hull of bboxes', () => {})
-  test('merge two bbox', () => {})
-  test('if a segment cross the node', () => {})
-  test('if two segments are intersected', () => {})
-  test('get the intersection of two segments', () => {})
-  test('get the intersection of a line segment and a rectangle', () => {})
-  test('get the intersections of cross lien of point and bbox', () => {})
-  test('if two bbox is overlapped', () => {})
-  test('get closest anchor of a node and a position', () => {})
-  test('if a point on the border of the rectangle', () => {})
-  test('get the intersections of cross line of point and circle', () => {})
-  test('get the height of the text in dom', () => {})
-})
+  test('get closest anchor', () => {
+    const position = { x: 0, y: 0 };
+    const node = {
+      anchors: [
+        { x: 0, y: 0, id: '1' },
+        { x: 1, y: 1, id: '2' },
+        { x: 2, y: 2, id: '3' },
+      ],
+    };
+    expect(getClosestAnchor(position, node)).toEqual({
+      anchor: { x: 0, y: 0, id: '1' },
+      index: 0,
+    });
+  });
+  test('is point inside the node', () => {
+    const position1 = { x: 0, y: 0 };
+    const position2 = { x: 110, y: 110 };
+    const node = {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+    };
+    expect(isInNode(position1, node)).toBeTruthy();
+    expect(isInNode(position2, node)).toBeFalsy();
+  });
+  test('get cross point with circle', () => {
+    const position = { x: 0, y: 0 };
+    const node = {
+      x: 0,
+      y: 0,
+      r: 10,
+    };
+    expect(getCrossPointWithCircle(position, 'horizontal', node)).toEqual({
+      x: 10,
+      y: 0,
+    });
+    expect(getCrossPointWithCircle(position, 'vertical', node)).toEqual({
+      x: 0,
+      y: 10,
+    });
+  });
+  test('get cross point with ellipse', () => {
+    const position = { x: 0, y: 0 };
+    const node = {
+      x: 0,
+      y: 0,
+      rx: 10,
+      ry: 5,
+    };
+    expect(getCrossPointWithEllipse(position, 'horizontal', node)).toEqual({
+      x: 10,
+      y: 0,
+    });
+    expect(getCrossPointWithEllipse(position, 'vertical', node)).toEqual({
+      x: 0,
+      y: 5,
+    });
+  });
+});
